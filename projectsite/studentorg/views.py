f3a4a49a62cc51e15ac69450c472b9a3b798d0ca
KILLERG565 <avancegabriel15@gmail.com>
2026-02-18 10:00:06 +0800

    updated views.py

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
+from .models import Item
 # Create your views here.
-from django.shortcuts import render
-
 def home(request):
-    return render(request, 'home.html')
+    items = Item.objects.all()  # Fetch all items
+    paginator = Paginator(items, 5)  # Show 5 items per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'home.html', {'page_obj': page_obj})
+    
